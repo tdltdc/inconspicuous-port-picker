e2b34d0814502fb799e796056ad097b93c6f509a
tdltdc <me@oraetlabora.club>
2023-11-17 18:32:01 +0100

    Fix old filename

--- random-port-picker.py
+++ random-port-picker.py
@@ -1,7 +1,10 @@
 #!/bin/python3
 import random
+import sys
+
+print(sys.argv)
 
 # Read port list from file and print random port
-port_list = "iana_unassigned_ports_list.txt"
+port_list = "inconspicuous_ports_list.txt"
 with open(port_list) as f:
     print(random.choice(f.read().splitlines()))
